7ee1ec9682bfde2fd605d9d9a6b166512aae7682
Shubham08pr <shubhamranasubhe@gmail.com>
2023-08-16 23:54:40 +0530

    slight changes

--- database/db.js
+++ database/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 export const Connection = async (username, password) => {
-  const URL = `mongodb+srv://${username}:${password}@ecommerce-web.fxtnwqk.mongodb.net/?retryWrites=true&w=majority`;
+  const URL =
+    process.env.MONGODB_URI ||
+    `mongodb+srv://${username}:${password}@ecommerce-web.fxtnwqk.mongodb.net/?retryWrites=true&w=majority`;
 
   try {
     await mongoose.connect(URL, {
