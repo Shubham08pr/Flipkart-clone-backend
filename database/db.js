c29474d52ddf3162afe9d971b378d8765b6c4516
Shubham08pr <shubhamranasubhe@gmail.com>
2023-09-26 00:50:56 +0530

    mongodb set to default path

--- database/db.js
+++ database/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 export const Connection = async (username, password) => {
-  const URL =
-    process.env.MONGODB_URI ||
-    `mongodb+srv://${username}:${password}@ecommerce-web.fxtnwqk.mongodb.net/?retryWrites=true&w=majority`;
+  // const URL =
+  //   process.env.MONGODB_URI ||
+  //   `mongodb+srv://${username}:${password}@ecommerce-web.fxtnwqk.mongodb.net/?retryWrites=true&w=majority`;
 
+  const URL = `mongodb+srv://${username}:${password}@ecommerce-web.fxtnwqk.mongodb.net/?retryWrites=true&w=majority`;
   try {
     await mongoose.connect(URL, {
       useUnifiedTopology: true,
